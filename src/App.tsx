9b666f783ec485265a4bca96fd126bb84cd0b418
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-08-14 23:00:36 +0600

    started exploring motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { motion } from "motion/react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="py-20 container mx-auto px-6">
+      <motion.h1
+        animate={{
+          x: [1000, 0,],
+        }}
+        transition={{
+          duration: 2, 
+          ease: "anticipate"
+        }}
+        className="text-2xl font-semibold"
+      >
+        Motion Playground...
+      </motion.h1>
+    </div>
   );
-}
+};
 
 export default App;
